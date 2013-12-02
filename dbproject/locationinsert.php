a22fe6a85141b52c0c73f9f87f747c02fa2da370
Joseph <Joseph@virginia.edu>
2013-12-02 18:30:33 -0500

    LocationsInster
    Works perfectly

---
+++ dbproject/locationinsert.php
@@ -0,0 +1,23 @@
+<?
+
+	include_once('dblogin.php');
+	
+       session_start();
+        
+	$db_connection = new mysqli($SERVER, $USER, $PASS, $DB);
+	if (mysqli_connect_error()) {
+		echo "Can't connect!";
+		echo "<br>" . mysqli_connect_error();
+		return null;
+	}
+
+    $startTime = $_POST['startTime'];
+    $startTime = mysqli_real_escape_string($db_connection ,$startTime);
+    //echo $startTime;
+    
+    $result = mysqli_query($db_connection,"INSERT INTO `cs4750apc5fr`.`locations`( `location_name`, `frequency`) VALUES ('$startTime',1)");
+    header('Location: teacherhome.html');
+    
+
+$db_connection->close();
+?>
